bf292b52dd1b1a82f5bdfccc145f72b3f6b2150d
caenrigen <31376402+caenrigen@users.noreply.github.com>
2019-01-21 13:33:39 +0100

    annoying prompts implemented

--- Menu.js
+++ Menu.js
@@ -1,38 +1,26 @@
 //  This function runs when a spreadsheet file is opened
 //  Adds options to menu, if the code is inside an add-on it will add the options in a submenu  in Add-ons
 
-function onOpen(e){
-  var ui = SpreadsheetApp.getUi();
-  var menuLabels = getDevSettings().getMenuLabels();
-  ui.createMenu('LatexKit')
-  .addItem(menuLabels['singleTabular'], 'menuMakeTabular')
-  .addItem(menuLabels['allTabular'], 'menuExportAllTabs')
-  .addSeparator()
-  .addItem(menuLabels['showSettings'], 'menuShowSettingsSheet')
-  .addItem(menuLabels['updateSettings'], 'updateSettingsSheet')
-  .addSeparator()
-  .addItem(menuLabels['singleData'], 'menuExportData')
-  .addItem(menuLabels['allData'], 'menuExportAllData')
-  .addSeparator()
-  .addItem('blabla', 'testNew')
-  .addToUi();
-}
+//function onOpen(e){
+//  var ui = SpreadsheetApp.getUi();
+//  var menuLabels = getDevSettings().getMenuLabels();
+//  ui.createMenu('LatexKit')
+//  .addItem(menuLabels['singleTabular'], 'menuMakeTabular')
+//  .addItem(menuLabels['allTabular'], 'menuExportAllTabs')
+//  .addSeparator()
+//  .addItem(menuLabels['showSettings'], 'menuShowSettingsSheet')
+//  .addItem(menuLabels['updateSettings'], 'updateSettingsSheet')
+//  .addSeparator()
+//  .addItem(menuLabels['singleData'], 'menuExportData')
+//  .addItem(menuLabels['allData'], 'menuExportAllData')
+//  .addSeparator()
+//  .addItem('blabla', 'testNew')
+//  .addItem('pop', 'testpop')
+//  .addItem('validadeKey', 'validateKeyPrompt')
+//  .addToUi();
+//}
 
 function onInstall(e){
   onOpen(e);
   // Perform additional setup as needed.
-}
-function test(){
-  Logger.log(getFormsUnlockKeys()["Y8BI1LXKBMJ6RZE8687U"]);
-}
-
-function testNew(){
-  showAnchor('Short Survey','https://goo.gl/forms/a5eweXqgkFmIX8092');
-}
-
-function showAnchor(name,url) {
-//  var html = '<html><body><a href="'+url+'" target="blank" onclick="google.script.host.close()">'+name+'</a></body></html>';
-  var html = '<html><body><a href="'+url+'" target="blank">'+name+'</a></body></html>';
-  var ui = HtmlService.createHtmlOutput(html)
-  SpreadsheetApp.getUi().showModelessDialog(ui,"demo");
 }
--- forms.js
+++ forms.js
@@ -1,3 +1,88 @@
+function onOpen(e){
+  var ui = SpreadsheetApp.getUi();
+  var menuLabels = getDevSettings().getMenuLabels();
+  ui.createMenu('LatexKit')
+  .addItem(menuLabels['singleTabular'], 'menuMakeTabularForm')
+  .addItem(menuLabels['allTabular'], 'menuExportAllTabsForm')
+  .addSeparator()
+  .addItem(menuLabels['showSettings'], 'menuShowSettingsSheetForm')
+  .addItem(menuLabels['updateSettings'], 'updateSettingsSheetForm')
+  .addSeparator()
+  .addItem(menuLabels['singleData'], 'menuExportDataForm')
+  .addItem(menuLabels['allData'], 'menuExportAllDataForm')
+  .addToUi();
+}
+
+function validateKeyPrompt(funcName){
+  var userProperties = PropertiesService.getUserProperties();
+  
+  //userProperties.deleteAllProperties();
+  
+  var myPropertyName = 'LateKitFormV1Filled';
+  if(userProperties.getProperty(myPropertyName) !== 'true'){
+    userProperties.setProperty(myPropertyName, 'false');
+    var ui = SpreadsheetApp.getUi();
+    var response = ui.prompt('Hello');
+    if(getFormsUnlockKeys()[response.getResponseText()]) userProperties.setProperty(myPropertyName, 'true');
+  }
+  var funcNames = {
+    menuMakeTabularFunc : menuMakeTabular,
+    menuExportAllTabsFunc : menuExportAllTabs,
+    menuShowSettingsSheetFunc : menuShowSettingsSheet,
+    updateSettingsSheetFunc : updateSettingsSheet,
+    menuExportDataFunc : menuExportData,
+    menuExportAllDataFunc : menuExportAllData
+  };
+  funcNames[funcName]();
+}
+
+function makeHTMLtext(funcName){
+  return '<!DOCTYPE html>\
+<html>\
+  <head>\
+    <base target="_top">\
+  </head>\
+  <body>\
+    <a href="https://goo.gl/forms/a5eweXqgkFmIX8092" target="blank">Something</a>\
+    <input type="button" value="OK" onclick="google.script.run.withSuccessHandler().validateKeyPrompt(\''+ funcName +'\')" />\
+  </body>\
+</html>'
+}
+
+function showSurveyAlert(funcName){
+  if(PropertiesService.getUserProperties().getProperty('LateKitFormV1Filled') !== 'true'){
+    var htmlOutput = HtmlService.createHtmlOutput(makeHTMLtext(funcName));
+    //.setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    //.setWidth(250)
+    //.setHeight(200);
+    SpreadsheetApp.getUi().showModelessDialog(htmlOutput, 'HELP');
+  } else validateKeyPrompt(funcName);
+}
+
+function menuMakeTabularForm(){
+  showSurveyAlert('menuMakeTabularFunc');
+}
+
+function menuExportAllTabsForm(){
+  showSurveyAlert('menuExportAllTabsFunc');
+}
+
+function menuShowSettingsSheetForm(){
+  showSurveyAlert('menuShowSettingsSheetFunc');
+}
+
+function updateSettingsSheetForm(){
+  showSurveyAlert('updateSettingsSheetFunc');
+}
+
+function menuExportDataForm(){
+  showSurveyAlert('menuExportDataFunc');
+}
+
+function menuExportAllDataForm(){
+  showSurveyAlert('menuExportAllDataFunc');
+}
+
 function getFormsUnlockKeys() {
   return {
     "YGF32PME51Z44JTJN3E3": true,
@@ -42,3 +127,26 @@ function getFormsUnlockKeys() {
     "Y8BI1LXKBMJ6RZE8687U": true
   }
 }
+//function test(){
+//  Logger.log(makeHTMLtext('menuMakeTabularFunc'));
+//}
+//
+//function testpop () {
+//  var htmlOutput = HtmlService
+//    .createHtmlOutputFromFile('FormsPopup2')
+//    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+//    .setWidth(250)
+//    .setHeight(200);
+//  SpreadsheetApp.getUi().showModalDialog(htmlOutput, 'HELP');
+//}
+//
+//function testNew(){
+//  showAnchor('testNew','https://goo.gl/forms/a5eweXqgkFmIX8092');
+//}
+//
+//function showAnchor(name,url) {
+////  var html = '<html><body><a href="'+url+'" target="blank" onclick="google.script.host.close()">'+name+'</a></body></html>';
+//  var html = '<html><body><a href="'+url+'" target="blank">'+name+'</a></body></html>';
+//  var ui = HtmlService.createHtmlOutput(html)
+//  SpreadsheetApp.getUi().showModelessDialog(ui,"demo");
+//}
